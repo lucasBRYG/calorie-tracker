9152987894f716d1964745b852fba33e04ce0196
lucasBRYG <adio.lucas@gmail.com>
2021-05-06 21:15:41 -0400

    basic styling of meal and workout cards

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,6 @@
 const mealCardButton = $("#mealCardButton");
 const exerciseCardButton = $("#workoutCardButton");
 
-const mealCard = `
-    <div class="meal__form">
-    <form>
-        <input type="text" id="meal">
-        <label for="meal">Meal</label>
-
-        <input type="number" id="calories">
-        <label for="calories">Calories in meal</label>
-
-        <input type="submit">
-    </form>
-</div>
-`
-
-const workoutCard = `
-    <div class="workout__form">
-        <form>
-            <input type="text" id="workout">
-            <label for="workout">Workout Name</label>
-
-            <input type="number" id="calories">
-             <label for="calories">Calories burned</label>
-
-            <input type="submit">
-         </form>
-    </div>
-`
-
 $(mealCardButton).on("click", () => {
     document.append();
 })
