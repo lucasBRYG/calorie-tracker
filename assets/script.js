d68bcfc291664eae8cae0b069f0bd711ae8d4c76
lucasBRYG <adio.lucas@gmail.com>
2021-05-17 17:51:25 -0400

    updated card styling

--- assets/script.js
+++ assets/script.js
@@ -8,12 +8,39 @@ $("#meal-submit").on("click", () => {
     console.log(meal + " " + calories);
     console.log("test2");
 
-    const mealCard = `
-        <div class = "meal__card">
-            <h4>${meal}</h4>
-            <h5>Calories</h5>
-            <p>${calories}</p>
-        </div>
-    `
-    $("#meal-cards").append(mealCard);
+    $("#meal-cards").append(createCard(meal, calories, "meal"));
+    
 })
+
+$("#workout-submit").on("click", () => {
+    let workout = $("#workout").val();
+    let calories = $("#workout__calories").val();
+
+    console.log(workout + " " + calories);
+    console.log("test2");
+
+    $("#workout-cards").append(createCard(workout, calories, "workout"));
+})
+
+function createCard(title, value, id) {
+
+    return `
+        <div class="${id}__card card">
+            <h4>${title}</h4>
+            <h5>Calories</h5>
+            <p>${value}</p>
+        </div>
+    `;
+    
+}
+
+// function testCards() {
+//     $("#meal-cards").append(createCard("Apple", 80, "meal"));
+//     $("#meal-cards").append(createCard("Apple", 80, "meal"));
+//     $("#meal-cards").append(createCard("Apple", 80, "meal"));
+//     $("#meal-cards").append(createCard("Apple", 80, "meal"));
+//     $("#meal-cards").append(createCard("Sando", 4200, "meal"));
+//     $("#workout-cards").append(createCard("Mile Run", 120, "workout"));
+// }
+
+// testCards();
