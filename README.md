9c86bd0a9f84ca7bb90430b856a8574da3339fc8
lucasBRYG <adio.lucas@gmail.com>
2021-06-11 12:07:56 -0400

    adding comments and updating readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ## Introduction
-Spring started 3 days ago, wich means the need for summer bods are around the corner. This will be a really basic weekly calorie tracker. It will track consumed calories and calories burned each day, and calculate how over/under a person's goal they are.
-
+This is a simple project I'm making mostly for my own use, but I think it will be a good excersize to see how much functionality I can add with the simplest tools. So I want to use jQuery, and basically no other frameworks or packages. Back to basics: HTML, CSS, and JS to handle everything. Try to keep everyghing it's own lane.
+And instead of a backend I want this application to rely on local storage as much as possible, because:
+ - Keeping it simple. No backend means no server management, no middleware, no view engines.
+ - Faster and more reliable
+ - MORE SECURE. Any data a user gives you, you are responsible for. Local storage keeps all a user's data close to them and limits vulnereability. User's might feel wierd about the idea that things are being stored locally so maybe a disclaimer would be good to add or maybe a way to let them look inside their own local storage.
+ - User Privacy. Again, since their's no centralization of their data, it doesn't have to move any farther than their own machine.
 ---
 
 ### Table of Contents
@@ -14,7 +18,7 @@ Spring started 3 days ago, wich means the need for summer bods are around the co
 ---
 
 ## Description
-Building a simple calorie tracker, mostly for myself but should be a good little project. Try it out here: https://lucasbryg.github.io/calorie-tracker/
+Summer bods are around the corner. This will be a really basic daily calorie tracker. It will track consumed calories and calories burned each day, and calculate how over/under a person's goal they are. Try it out here: https://lucasbryg.github.io/calorie-tracker/
 
 ### MVP
 This is a very simple application, intended for personal use(at first). Soon I want to build in some more functinality like a progress bar, and tracking calorie intake over weeks. For now though, here's what I want it to do:
@@ -30,29 +34,42 @@ I WANT a way to keep track of meals ive eaten and workouts ive completed
 SO THAT I can plan and track my health and fitness progress
 ```
 
-### Flex Goals
- - Add a progress bar that fills up as you consume calories. 100% will be the daily intake goal.
+### Stretch Goals
  - Track Daily Net Calories over the week.
  - Add an info section with tips on how to use and meet fitness goals
- - A way to automatically calculate the calories in a meal or workout based on info from user; ie. Duration of workout, type of excersize, or what the meal was and how much they ate
+ - A way to automatically calculate the calories in a meal or workout based on info from user; ie. Duration of workout, type of excersize, or what the meal was and how much they ate. This can probably be done with an API
+ - Possibly turn into a PWA, once more assets like logos and graphics are working
+---
 
 ## How to Use
 When you first visit the webpage, you should see a prompt asking your goal for daily calorie consumption. NOTE: This number will be different for everyone. Please consult with your physician or nutritionist for this number in order to get the safest and best results.
-Then you can add meals or workouts using the two forms on the right. Cards with the info you provide will appear in the page, and your Net Calories, at the bottom of the page, will change as you do. The page uses local storage to store and retrieve information you put in, so you can come back later the same day to add or edit your progress.
+Then you can add meals or workouts using the two forms on the right. Cards with the info you provide will appear in the page, and your Net Calories, at the bottom of the page, will change as you do. The page uses local storage to store and retrieve information you put in, so you can come back later the same day to add or edit your progress If you have local storage or javascript disable this application will not work for you.
 PLEASE USE RESPONSIBLY
+
+---
 
 ## Technologies
 This application was made using HTML5, CSS3, ES6 JavaScript, and jQuery for accessing local storage and manipulating the DOM.
 
+---
+
 ## Screenshots
 ### Full page
-![Full page view of application](https://github.com/lucasBRYG/calorie-tracker/blob/main/assets/images/Screenshot%202021-06-07%20130224.png)
+![Full page view of application](https://github.com/lucasBRYG/calorie-tracker/blob/main/assets/images/fullpageview.png)
 
 ### Meal Section
 ![View of meal section](https://github.com/lucasBRYG/calorie-tracker/blob/main/assets/images/Screenshot%202021-06-07%20130139.png)
 
 ### Net Calories
 ![View of net calories tracker](https://github.com/lucasBRYG/calorie-tracker/blob/main/assets/images/Screenshot%202021-06-07%20130245.png)
+
+### Progress Wheel
+![View of progress wheel](https://github.com/lucasBRYG/calorie-tracker/blob/main/assets/images/progress-wheel.png)
+
+### Over Wheel
+![View of over-progress wheel](https://github.com/lucasBRYG/calorie-tracker/blob/main/assets/images/over-wheel.png)
+
+---
 
 ## Author
  Lucas Santiago
--- assets/script.js
+++ assets/script.js
@@ -1,180 +1,179 @@
-const mealForm = $(".meal__form");
-const exerciseForm = $(".workout__form");
-const todayNetEl = $("#today-net");
-const weekNetEl = $("#week-net");
-const progressCirlce = $(".progress");
-const overCircle = $(".over-progress")
+const mealForm = $(".meal__form");          // variable references meal form on page
+const exerciseForm = $(".workout__form");   // variable references workout form on page
+const todayNetEl = $("#today-net");         // variable references Today Net element on page
+const weekNetEl = $("#week-net");           // variable references Week Net element (not used rn)
+const progressCirlce = $(".progress");      // variable references green progess wheel on page
+const overCircle = $(".over-progress")      // variable references red over-progress wheel on page
 
-$(document).ready(() => {
+$(document).ready(() => { // when document has loaded and ready on page, execute the following...
 
-    if(localStorage.getItem("goal")) {
+    if(localStorage.getItem("goal")) {  // checks in local storage has a value for goal key
 
-        if(localStorage.getItem("day") != new Date().getDate()) {
-            localStorage.setItem("day", new Date().getDate());
-            localStorage.setItem("day-calories", JSON.stringify({calories: 0}));
-            localStorage.setItem("day-meals", "[]");
-            localStorage.setItem("day-workouts", "[]");
+        if(localStorage.getItem("day") != new Date().getDate()) {   // checks if day key holds a value that is different than the current date. If not today, resets items in local storage
+            localStorage.setItem("day", new Date().getDate());  // sets day value to todays date (holds integer from 1-31)
+            localStorage.setItem("day-calories", JSON.stringify({calories: 0}));    // sets day-calories to zero (inside an objest for scalability)
+            localStorage.setItem("day-meals", "[]");    // sets day meals to empty array
+            localStorage.setItem("day-workouts", "[]"); //sets day workouts to empty array
+        }  //chekcing week value in ls  //get current month, add one(0 indexed)  //gets current date, subtracts day of week to get start of current week
+        if(localStorage.getItem("week") != `${new Date().getMonth() + 1}/${new Date().getDate() - new Date().getDay()}`) {  // checks to see if week key in local storage is different from current week. If not, resets items in local storage.
+            const week = `${new Date().getMonth() + 1}/${new Date().getDate() - new Date().getDay()}`;  // creates const that holds string of current week start date
+            localStorage.setItem("week", week); // sets week value to local storage
+            localStorage.setItem("week-calories",  JSON.stringify({calories: 0}));  // resets week calories in ls
+            localStorage.setItem("day-meals", "[]");    // resets day meals. That way if its the same day in a new month or year it will still reset
+            localStorage.setItem("day-workouts", "[]"); // resets day workotus. "" "" "" "" 
         }
-        if(localStorage.getItem("week") != `${new Date().getMonth() + 1}/${new Date().getDate() - new Date().getDay()}`) {
-            const week = `${new Date().getMonth() + 1}/${new Date().getDate() - new Date().getDay()}`;
-            localStorage.setItem("week", week);
-            localStorage.setItem("week-calories",  JSON.stringify({calories: 0}));
-            localStorage.setItem("day-meals", "[]");
-            localStorage.setItem("day-workouts", "[]");
-        }
 
-        todayNetEl.html(`${JSON.parse(localStorage.getItem("day-calories")).calories-JSON.parse(localStorage.getItem("goal"))}`);
-        renderCards();
+        todayNetEl.html(`${JSON.parse(localStorage.getItem("day-calories")).calories-JSON.parse(localStorage.getItem("goal"))}`);   // once the resets are finished, display value of day calories to page as difference of (goal+calsBurned)-cals eaten. (use to score day)
+        renderCards();  // render meals and workout cards 
 
-    } else {
-        const goal = prompt("Enter your daily calorie intake goal:");
-        const week = `${new Date().getMonth() + 1}/${new Date().getDate() - new Date().getDay()}`;
-        const day = `${new Date().getDate()}`;
-        localStorage.setItem("goal", goal);
-        localStorage.setItem("week", week);
-        localStorage.setItem("day", day);
-        localStorage.setItem("day-calories",  JSON.stringify({calories: 0}));
-        localStorage.setItem("week-calories",  JSON.stringify({calories: 0}));
-        localStorage.setItem("day-meals", "[]");
-        localStorage.setItem("day-workouts", "[]");
+    } else {    // If user hasn't been to page or has empty local storage
+        const goal = prompt("Enter your daily calorie intake goal:");   // gets users calorie intake goal and stores it in goal const
+        const week = `${new Date().getMonth() + 1}/${new Date().getDate() - new Date().getDay()}`;  // sets week const to date of the start of the current week
+        const day = `${new Date().getDate()}`;  // sets date const to current date
+        localStorage.setItem("goal", goal); // sets goal in ls to goal const val
+        localStorage.setItem("week", week); // sets week in ls to week const val
+        localStorage.setItem("day", day);   // sets day in ls to week const val
+        localStorage.setItem("day-calories",  JSON.stringify({calories: 0}));   // sets day cals in ls to 0
+        localStorage.setItem("week-calories",  JSON.stringify({calories: 0}));  // sets week cals in ls to 0 (not used rn)
+        localStorage.setItem("day-meals", "[]");    // sets day meals to empty array
+        localStorage.setItem("day-workouts", "[]"); // sets day workotus to emtpty array
 
-        todayNetEl.html(`${JSON.parse(localStorage.getItem("day-calories")).calories}`);
-        renderCaloriesBurned();
+        todayNetEl.html(`${JSON.parse(localStorage.getItem("day-calories")).calories}`);    // display value of day calories to page as difference of (goal+calsBurned)-cals eaten. (use to score day) (starts 0)
+        renderCaloriesBurned(); // displays total calories burned to page (starts 0)
         
     }
 
-});
+}); // end of doc ready function
 
 
-$("#meal-submit").click(() => {
-    let mel = $("#meal").val();
-    let calories;
-    if ($("#meal__calories").val() != '') {
-        calories = $("#meal__calories").val();
-    } else {
-        calories = 0;
+$("#meal-submit").click(() => { // adds on click event to meal form submit button
+    let meal = $("#meal").val(); // takes in value of meal name input section
+    let calories;   // declares calories variable
+    if ($("#meal__calories").val() != '') { // checks if calories field in form has a value
+        calories = $("#meal__calories").val();  // calories variable holds val in calories field
+    } else {    // if no value
+        calories = 0;   // calies set to 0
     }
 
-    $("#meal-cards").append(createCard(mel, calories, "meal"));
-    setDayCalLS(calories);
+    $("#meal-cards").append(createCard(meal, calories, "meal"));    // add new element to meal-cards sectino on page. createCard() return a div element litteral string
+    setDayCalLS(calories);  // updates calories value in local storage
 
-    setDayMeals({meal: mel, calories: calories});
-    renderUpdate();
+    setDayMeals({meal: meal, calories: calories});  // updates local storage to include new meal
+    renderUpdate(); // updates all elements on page that need to be rendered
 });
 
-$("#workout-submit").click(() => {
-    let wrkt = $("#workout").val();
-    let calories = 0;
-    if ($("#workout__calories").val() != null) {
-        calories -= $("#workout__calories").val();
+$("#workout-submit").click(() => { // adds onclick event to workout submit button
+    let wrkt = $("#workout").val(); // declares and initializes wrkt variable to value of workout name in form
+    let calories = 0;   // declares calories variable and sets it to zero
+    if ($("#workout__calories").val() != null) { //only effects calories variable if calore field is NOT null in workout form
+        calories -= $("#workout__calories").val();  // calories is negative number
     }
 
-    $("#workout-cards").append(createCard(wrkt, calories, "workout"));
-    setDayCalLS(calories);
+    $("#workout-cards").append(createCard(wrkt, calories, "workout"));  // add new element to workout-cards sectino on page. createCard() return a div element litteral string
+    setDayCalLS(calories);  // updates calories value in local storage
 
-    setDayWorkouts({workout: wrkt, calories: calories});
-    renderUpdate();
+    setDayWorkouts({workout: wrkt, calories: calories});    // updates local storage to include new meal
+    renderUpdate(); // updates all elements on page that need to be rendered
 });
 
-function createCard(title, value, id) {
+function createCard(title, value, id) { // function generates litteral string elements populated with info passed through parameters
 
     return `
-        <div class="${id}__card card" data-type="${id}">
+        <div class="${id}__card card" data-type="${id}">    
             <h4>${title}</h4>
             <p class="card-calorie-section">Calories: <span>${value}</span></p>
             <button class = "delete-button" onClick="deleteButton(event)">Delete</button>
         </div>
-    `;
+    `;  // div elemement with card class for styling and data type set to either 'meal' or 'workout' title and value are the name of the workout/meal and calories value. Delete button initialized with onclick function
 }
 
-function setDayCalLS(newCals) {
-    let calories = JSON.parse(localStorage.getItem("day-calories"));
-    calories.calories = parseInt(calories.calories);
-    calories.calories += parseInt(newCals);
+function setDayCalLS(newCals) { // function updates calories in ls. newCals param is an int
+    let calories = JSON.parse(localStorage.getItem("day-calories")); // new calories vaiable (object{'calories': 'int'})
+    calories.calories = parseInt(calories.calories);    // changes caloreis value in object to an ineger variabel type
+    calories.calories += parseInt(newCals); // updates calories by adding calories from +meal or -workout
 
-    localStorage.setItem("day-calories", JSON.stringify(calories));
+    localStorage.setItem("day-calories", JSON.stringify(calories)); // sets new object to local storage
 }
 
-function setDayWorkouts(workout) {
-    let workouts = JSON.parse(localStorage.getItem("day-workouts"));
-    workouts.push(workout);
-    localStorage.setItem("day-workouts", JSON.stringify(workouts));
+function setDayWorkouts(workout) {  // function updates array of workouts in local storage with new workout. workout param is an object
+    let workouts = JSON.parse(localStorage.getItem("day-workouts"));    // new workouts variable holds array of objects
+    workouts.push(workout); // pushes workout object from params to end of workouts array
+    localStorage.setItem("day-workouts", JSON.stringify(workouts)); // updates ls to include new workout
 }
 
-function setDayMeals(meal) {
-    let meals = JSON.parse(localStorage.getItem("day-meals"));
-    meals.push(meal);
-    localStorage.setItem("day-meals", JSON.stringify(meals));
+function setDayMeals(meal) {    // function updates array of meals in local storage. meal passed to params is ab object
+    let meals = JSON.parse(localStorage.getItem("day-meals"));  // new meals variable holds array of meal objects from ls
+    meals.push(meal);   // push meal object to end of meals array
+    localStorage.setItem("day-meals", JSON.stringify(meals));   // update meals in local storage to include new meal
 }
 
-function deleteButton(e, i) { // e will be a js event object.
-    let target = e.target.parentNode;
-    let targetParent = target.parentNode;
-    let targetType = $(e.target.parentNode).attr("data-type")
-    let arrayLocal = JSON.parse(localStorage.getItem(`day-${targetType}s`));
+function deleteButton(e) { // e will be a js event object.
+    let target = e.target.parentNode;   // new target variable set to reference the card that is to be deleted
+    let targetParent = target.parentNode;   // new targetParent variable set to reference parent container of selected card
+    let targetType = $(e.target.parentNode).attr("data-type")   // sets targetType variable to data-type attribute of selected element. Either 'meal' or 'workout'
+    let arrayLocal = JSON.parse(localStorage.getItem(`day-${targetType}s`));    // new arrayLocal variable to value of local storage item; either 'day-meals' or 'day-workouts'
 
-    let tpChildren = []
-    for(let i=0; i < targetParent.children.length; i++) {
-        tpChildren.push(targetParent.children[i]);
+    let tpChildren = [] // tpChildren set to empty array. This will hold the div elements inside parent container so es6 array protos can be used on it
+    for(let i=0; i < targetParent.children.length; i++) {   // for each element inside parent container... 
+        tpChildren.push(targetParent.children[i]);  // add each child to tpChildren
     }
 
-    let targetIndex = tpChildren.indexOf(target);
-    setDayCalLS(-arrayLocal[targetIndex].calories);
+    let targetIndex = tpChildren.indexOf(target);   // targetIndex holds index value of target in relation to parent
+    setDayCalLS(-arrayLocal[targetIndex].calories); // subtracts calorie value in element from local storage
 
-    arrayLocal.splice(targetIndex, 1);
-    localStorage.setItem(`day-${targetType}s`, JSON.stringify(arrayLocal ));
-    target.remove();
-    renderUpdate();
+    arrayLocal.splice(targetIndex, 1);  // splice removes items from array from specified index(1st param), up to number of elements specified(2nd param)
+    localStorage.setItem(`day-${targetType}s`, JSON.stringify(arrayLocal ));    // updates array of either workouts or meals in local storage
+    target.remove();    // remove target element from dom
+    renderUpdate();     // re-renders items on page
 }
 
-function renderUpdate() {
+function renderUpdate() {   // function renders mutable page elemnts when called
 
     todayNetEl.html(`${JSON.parse(localStorage.getItem("day-calories")).calories-JSON.parse(localStorage.getItem("goal"))}`);
-    renderCaloriesBurned();
-    progressBarLogic();
+    renderCaloriesBurned(); // renders calories bunred section of page
+    progressBarLogic(); // calculates and renders progress wheel
 }
 
-function renderCards() {
-    let meals = JSON.parse(localStorage.getItem("day-meals"));
-    let workouts = JSON.parse(localStorage.getItem("day-workouts"));
+function renderCards() {    // functino renders all cards from local storage to page
+    let meals = JSON.parse(localStorage.getItem("day-meals"));  // new meals variable is set to meals array from local storage
+    let workouts = JSON.parse(localStorage.getItem("day-workouts"));    // new workouts variable is set to workouts array from ls
 
-    meals.forEach(meal => {
-        $("#meal-cards").append(createCard(meal.meal, meal.calories, "meal"));
+    meals.forEach(meal => { // for each meal in array...
+        $("#meal-cards").append(createCard(meal.meal, meal.calories, "meal"));  // create card and add it to meal-cards section of page
     });
-    workouts.forEach(workout => {
-        $("#workout-cards").append(createCard(workout.workout, workout.calories, "workout"));
+    workouts.forEach(workout => {   // for each workout in array
+        $("#workout-cards").append(createCard(workout.workout, workout.calories, "workout"));   // create card and add it to meal-cards section of page
     })
-    renderUpdate();
+    renderUpdate(); // re-render page elements to match meal and workout cards 
 }
 
-function renderCaloriesBurned() {
-    let calsBurned = 0;
-    let workouts = JSON.parse(localStorage.getItem("day-workouts"));
-    workouts.forEach(workout => {
-        calsBurned += -workout.calories;
+function renderCaloriesBurned() {   // function calculates total calories burned value and renders to page
+    let calsBurned = 0; // new calsBurned variabel set to 0
+    let workouts = JSON.parse(localStorage.getItem("day-workouts"));    // new workotus variable set to array of workouts from ls
+    workouts.forEach(workout => {   // for each workout in array...
+        calsBurned += -workout.calories;    // add calories in workout to calsBurned
     });
 
-    $("#calories-burned-val").html(calsBurned);
+    $("#calories-burned-val").html(calsBurned); // renders value in calories-burned-val page element
 }
 
-function progressBarLogic() {
-    let circumference = 440;
-    let caloriesMax = JSON.parse(localStorage.getItem("goal"));
-    let currentCals = 0;
-    let over = 0;
-    let meals = JSON.parse(localStorage.getItem("day-meals"));
-    meals.forEach(meal => {
-        currentCals += parseInt(meal.calories);
+function progressBarLogic() {   // function calculates how much of wheel to fill and renders to page
+    let circumference = 440;    // circumference of wheel in pixels
+    let caloriesMax = JSON.parse(localStorage.getItem("goal")); // gets goal from local storage to use as max
+    let currentCals = 0;    // new currentCals variable set to 0. will hold calories consumed so far today
+    let over = 0;           // new over variable set to 0. will hold how many caloreis over goal user is
+    let meals = JSON.parse(localStorage.getItem("day-meals"));  // new meals variable set to array from ls
+    meals.forEach(meal => { // for each meal in array...
+        currentCals += parseInt(meal.calories); // add calories from each meal to currentCals
     });
-    if ((currentCals/caloriesMax)*circumference > circumference) {
-        over = ((currentCals/caloriesMax)*circumference)-circumference;
-        if (over > circumference) {
-            over = circumference;
+    if ((currentCals/caloriesMax)*circumference > circumference) {  // evaluates if calories are over user's goal (as a ratio)
+        over = ((currentCals/caloriesMax)*circumference)-circumference; // sets over to how many calories over the user's goal they are
+        if (over > circumference) { // evaluates if over is greater than circumference of wheel
+            over = circumference;   // if so just have over be value of circumference
         }
     }
-    $(progressCirlce).css(`stroke-dashoffset`, `${circumference-((currentCals/caloriesMax)*circumference)+over}px`);
-    $(".progress-lable").html(`${currentCals}/${caloriesMax}`);
-    over = 
-    $(overCircle).css(`stroke-dashoffset`, `${circumference-over}`);
+    $(progressCirlce).css(`stroke-dashoffset`, `${circumference-((currentCals/caloriesMax)*circumference)+over}px`);    // sets stroke-dashoffset attribute of progress wheel to ratio of wheel to be filled * circumferece
+    $(".progress-lable").html(`${currentCals}/${caloriesMax}`); // sets label of progress wheel and renders
+    $(overCircle).css(`stroke-dashoffset`, `${circumference-over}`);    // sets stoke-dashoffet attribute of over-circle element on page
 
 }
